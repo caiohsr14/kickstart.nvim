40cfd62c2d90a4e9e4cd13dd442a76f745adbed3
Caio Rodrigues <caio.rodrigues@humblebundle.com>
2024-10-14 20:47:02 -0300

    add undotree

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -302,7 +302,12 @@ require('lazy').setup({
   {
     'mg979/vim-visual-multi',
   },
-
+  {
+    'mbbill/undotree',
+    config = function()
+      vim.keymap.set('n', '<leader>u', vim.cmd.UndotreeToggle)
+    end,
+  },
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
