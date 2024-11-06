c3dcb1e37ae322f91256229545dac20dafa1f808
Caio Rodrigues <caio.rodrigues@humblebundle.com>
2024-11-06 14:06:01 -0300

    snippets

--- init.lua
+++ init.lua
@@ -204,8 +204,8 @@ vim.keymap.set('n', 'J', 'mzJ`z')
 vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 vim.keymap.set('n', 'n', 'nzzzv')
-
 vim.keymap.set('n', 'N', 'Nzzzv')
+
 vim.keymap.set('x', '<leader>p', [["_dP]])
 
 vim.keymap.set('n', 'Q', '<nop>')
@@ -717,12 +717,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          {
+            'rafamadriz/friendly-snippets',
+            config = function()
+              require('luasnip.loaders.from_vscode').lazy_load()
+            end,
+          },
         },
       },
       'saadparwaiz1/cmp_luasnip',
